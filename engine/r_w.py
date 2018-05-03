76e8960e9ea7669d7ca727aa43a4757570b4022e
rayraib <binitarai11@gmail.com>
2018-05-03 21:12:29 +0000

    re-factor code to implement second level relation

--- engine/r_w.py
+++ engine/r_w.py
@@ -9,12 +9,13 @@ from search import *
 
 def read_input():
     ''' Read and write to JSON file'''
+    '''
     with open('../actors.json', 'r') as f:
         data = json.load(f)
         actor1 = data['actor1']
         actor2 = data['actor2']
-
-    result = call_api(actor1, actor2)
+    '''
+    result = call_api('Kevin Bacon', 'Jim Carrey')
 
     with open('../result.json', 'w') as f:
         json.dump(result, f)
--- engine/search.py
+++ engine/search.py
@@ -3,42 +3,43 @@
 import requests
 
 def call_api(a1, a2):
-    '''calls the movies api to get the actors and movies'''
-    name = a1.replace(" ", "+")
-    url = "http://www.theimdbapi.org/api/find/person?name={}".format(name)
-    results = requests.get(url)
-    contents = results.json()[0]
-    try:
-        films = contents["filmography"]["actor"]
-    except KeyError:
-        films = contents["filmography"]["actress"]
+    ''' Entry point for two actors to find their degree of relationship'''
+    r = []
+    mlist1 = get_movies(a1)
+    mlist2 = get_movies(a2)
 
-    list_films = {}
-    for movie in films:
-        if movie["type"] == "Film":
-            list_films[movie["title"]] = movie["imdb_id"]
+    result = compare(mlist1, mlist2, a1, a2)
+    if result is not False:
+        print('found level 1')
+        return result
+    for movie, value in mlist1.items():
+        cast = get_actors(value)
+        for person in cast:
+            if person['name'] == a2:
+                continue
+            person_movies = get_movies(person['name'])
+            result = compare(person_movies, mlist2, person['name'], a2)
+            if result is not False:
+                lev1 = {"name1": a1, 
+                        "movie": [movie],
+                        "name2": person['name'],
+                        "error": False}
+                r.append(lev1)
+                r.append(result)
+                return r
 
-    name2 = a2.replace(" ", "+")
-    url = "http://www.theimdbapi.org/api/find/person?name={}".format(name2)
-    results = requests.get(url)
-    contents = results.json()[0]
-    try:
-        films = contents["filmography"]["actor"]
-    except KeyError:
-        films = contents["filmography"]["actress"]
-    list_films2 = {}
-    for movie in films:
-        if movie["type"] == "Film":
-            list_films2[movie["title"]] = movie["imdb_id"]
 
+def compare(list_films, list_films2, name, a2):
+    ''' Compare set of two movies and check if there is common element '''
     if set(list_films2.keys()) & set(list_films.keys()):
         results = list(set(list_films2.keys()).intersection(list_films.keys()))
-        return [{"name1": name.replace("+", " "),
+        return {"name1": name, 
                  "movie": results,
-                 "name2": name2.replace("+", " "),
-                 "error": False}]
+                 "name2": a2,
+                 "error": False}
     else:
-        print ("nothing")
+        return (False)
+
 
 def get_movies(actor):
     ''' 
@@ -47,6 +48,7 @@ def get_movies(actor):
     '''
 
     name = actor.replace(" ", "+")
+    print(name)
     url = "http://www.theimdbapi.org/api/find/person?name={}".format(name)
     results = requests.get(url)
     contents = results.json()[0]
@@ -61,7 +63,14 @@ def get_movies(actor):
             list_films[movie["title"]] = movie["imdb_id"]
     return (list_films)
 
+def get_actors(movie):
+    ''' Make API call and return all the cast for a movie '''
+    url = "http://www.theimdbapi.org/api/movie?movie_id={}".format(movie)
+    results = requests.get(url)
+    contents = results.json()['cast']
+    return (contents)
+
 
 if __name__ == "__main__":
-    call_api("brad pitt", "angelina jolie")
-    call_api("Meryl Streep","Daniel Day Lewis")
+    # call_api("brad pitt", "angelina jolie")
+    # r = call_api("Meryl Streep","Daniel Day Lewis")

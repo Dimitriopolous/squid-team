63961b9c7c07426cd4576249d93e88976385e676
jagrvargen <jagarthewolf@gmail.com>
2018-05-03 23:39:12 +0000

    emails are sending but without pdf attachment functionality

--- engine/email.py
+++ engine/email.py
@@ -3,8 +3,12 @@
    This module contains the functionality to retrieve and save user names and
    emails/
 '''
-import email
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.application import MIMEApplication
 import json
+import PyPDF2
 import smtplib
 import uuid
 
@@ -22,8 +26,8 @@ class Email():
     def __init__(self, *args, **kwargs):
         ''' This will initiate an email object instance. '''
         self.id = str(uuid.uuid4())
-        if "first_name" not in kwargs or "last_name" not in kwargs or "email"
-        not in kwargs:
+        if "first_name" not in kwargs or "last_name" not in kwargs or "email"\
+           not in kwargs:
             return True
         for key, value in kwargs.items():
             setattr(self, key, value)
@@ -44,14 +48,23 @@ class Email():
 
     def send(self):
         ''' Sends emails to all users '''
-        msg = "Hi!!!!!"
+        msg = MIMEMultipart()
+
+        msg["Subject"] = "Hey Friend!"
+        msg["From"] = "searchsquid@gmail.com"
+
+        body = "Find the Path of Relationships Between Actors\nEver wonder if two of your favorite actors starred in the same movie?\nSquid Search has your answers!\nWe’re an online search engine that allows users to input the names of two actors and find out how many degrees of separation there are between them through the movies that they’ve acted in. Squid Search can help you discover movies that you might love, follow your favorite star’s creative works, or feed into your curiosity about which actors have worked together."
+
         server.starttls()
-        server.login("searchsquid", "squidsquad7")
+        server.login("searchsquid@gmail.com", 'squidsquad7')
+        from_addr = "searchsquid@gmail.com"
 
         email_list = self.load()
         for item in email_list.values():
+            msg["To"] = item["email"]
+            msg.attach(MIMEText(body, 'plain'))
             send_to = item["email"]
-            server.sendmail("searchsquid@gmail.com", send_to, msg)
+            server.sendmail("searchsquid@gmail.com", send_to, msg.as_string())
         server.quit()
 
     def load(self):

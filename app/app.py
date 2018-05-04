d967a98ee49532c6f4b1dc1ba25f82a51c9c4acb
squidcarroll <squidcarroll@gmail.com>
2018-05-03 21:40:03 -0700

    push new route

--- app/app.py
+++ app/app.py
@@ -4,15 +4,7 @@ app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def hello_world():
-    res = []
-    try:
-        with open("../test.json", 'r') as f:
-            res = json.load(f)
-    except FileNotFoundError:
-        res = { "error": "File Not Found" }
-    except json.decoder.JSONDecodeError:
-        res = "This is not a json"
-    return render_template('index.html', movie_list=res)
+   return render_template('index.html')
 
 @app.route('/data', methods=['POST'])
 def handle_data():
@@ -21,7 +13,19 @@ def handle_data():
     with open('../actors.json', "w") as f:
         a_dict = { "actor1": a1, "actor2": a2 }
         json.dump(a_dict, f)
-    return redirect('/')
+    return redirect('/result')
+
+@app.route('/result')
+def display_result():
+    res = []
+    try:
+        with open("../result.json", 'r') as f:
+            res = json.load(f)
+    except FileNotFoundError:
+        res = { "error": "File Not Found" }
+    except json.decoder.JSONDecodeError:
+        res = "This is not a json"
+    return render_template('results.html', movie_list=res
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')

525e4cca1d16c0d6d246ea7ef15c4551e0f3b371
squidcarroll <squidcarroll@gmail.com>
2018-05-03 20:11:16 -0700

    push fix for css and js

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,11 +1,11 @@
 function submitVals() {
-	let file = File("../../actors.json");
+	let file = new File("../../../actors.json");
 	let arr = [];
 	arr.push({
 		"actor1": getElementsByName("actor1").value,
 		"actor2": getElementsByName("actor2").value
-	);
+	});
 	file.open("w");
-	file.writeIn(arr);
+	file.write(arr);
 	file.close();
 }
